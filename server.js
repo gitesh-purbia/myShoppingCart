30752c66b41be2331a30fc2accc399d2bb93765f
Gitesh Purbia <gitesh.purbiya22@gmail.com>
2018-02-26 18:55:24 +0530

    update server file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var serverFactory = require('spa-server');
 
 var server = serverFactory.create({
   path: './dist',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 5000,
   fallback: 'index.html'
 });
 
