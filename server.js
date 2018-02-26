9deed87b0e71c187f03ea2162cb731f634ee07b8
Gitesh Purbia <gitesh.purbiya22@gmail.com>
2018-02-26 21:20:41 +0530

    update server

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-var serverFactory = require('spa-server');
-
-var server = serverFactory.create({
-  path: './dist',
-  port: process.env.PORT || 5000,
-  fallback: 'index.html'
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
-server.start();
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 3000);
